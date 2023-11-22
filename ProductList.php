62e9db2d79e587baa4eccb8fd08389bff1989a67
Vigneswari <b1801936@helplive.edu.my>
2023-11-23 02:58:50 +0800

    Displays productsList and Pay Amount

--- ProductList.php
+++ ProductList.php
@@ -1,55 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Dashboard</title>
     <style>
         body {
-            background-color: #d6d1f0; /* Change this to your preferred background color */
+            margin: 0;
+            padding: 0;
+            background-color: white;
         }
         .header {
             text-align: center;
-            background-color: #333; /* Add your desired background color */
-            color: white; /* Add your desired text color */
-            padding: 20px; /* Add padding to create space around the content */
-        }
-        .header h1 {
-            margin: 0;
-            padding: 0;
+            background-color: #333;
+            color: white;
+            padding: 20px;
         }
         .menu-link {
             text-align: center;
-            background-color: black; /* Add your desired background color for the menu link */
-            padding: 10px; /* Adjust padding as needed */
-        }
-        .menu-link a {
-            color: white; /* Change the color of the menu link text to red */
-            text-decoration: none; /* Remove underlines from the link */
-            padding: 10px 20px; /* Add padding to the link (top and bottom, left and right) */
-            margin: 5px; /* Add margin to create space around the link */
+            background-color: black;
+            padding: 12px;
         }
         .table-container {
-            margin: 15px;
+            margin-top: 12px;
             text-align: center;
         }
         table {
-            border: 3px solid #000;
+            border: 4px solid #000;
             display: inline-block;
+            background-color: rgba(255, 255, 255, 0.8);
         }
         table th, table td {
-            padding: 10px;
+            padding: 9px;
             text-align: left;
         }
-        .social-links {
-            text-decoration: none;
-            color: #fff;
-            margin: 10px;
-            padding: 10px 10px; /* Add padding to the link (top and bottom, left and right) */
-        }
         .footer {
-            color: #fff;
             text-align: center;
-            background-color: black; /* Add your desired background color for the menu link */
-            padding: 0px; /* Adjust padding as needed */
+            background-color: #333;
+            color: white;
+            margin-top: 325px;
+        }
+        .footer a {
+            color: white;
+            text-decoration: none;
+            padding: 0 15px;
         }
     </style>
 </head>
@@ -60,92 +54,83 @@
 
     <div class="menu-link">
         <a href="AddProduct.html">Back</a>
-        <a href="TotalAmount.php">Next</a>
-        <a href="Login.php">Log out</a>
+        <a href="TheGetAwayLogin.php">Log out</a>
     </div>
 
-<div class="table-container">
-    <table>
-        <thead>
-            <tr>
-                <th>Product ID</th>
-                <th>Product Name</th>
-                <th>Product Description</th>
-                <th>Product Cost</th>
-                <th>Product Quantity</th>
-                <th>Product Image</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="table-container">
+        <form method="POST" action="TotalAmount.php"> <!-- Update the action attribute here -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Product Name</th>
+                        <th>Product Description</th>
+                        <th>Product Amount</th>
+                        <th>Product Image</th>
+                        <th>Quantity</th>
+                        <th>Payment Type</th>
+                        <th>Purchase Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-            <?php
-            session_start();
-            
-            // Connect to your database (replace with your database credentials)
-            $conn = mysqli_connect("localhost:3310", "root", "", "manage_tourism_product");
-            // Check the connection
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+                <?php
+                session_start();
 
-            // Fetch product data from the database
-            $sql = "SELECT * FROM tbl_product";
-            $result = mysqli_query($conn, $sql);
+                // Check if form is submitted
+                if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                    // Retrieve selected product details from POST data
+                    $selectedProductID = isset($_POST['ProductID']) ? $_POST['ProductID'] : '';
+                    $selectedProductName = isset($_POST['ProductName']) ? $_POST['ProductName'] : '';
+                    $selectedProductDescription = isset($_POST['ProductDescription']) ? $_POST['ProductDescription'] : '';
+                    $selectedProductCost = isset($_POST['ProductAmount']) ? $_POST['ProductAmount'] : '';
+                    $selectedProductImage = isset($_POST['ProductImage']) ? $_POST['ProductImage'] : '';
 
-            // Check the connection
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $productID = $row['ProductID'];
-                    $paymentMethod = isset($_SESSION['payment_methods'][$productID]) ? $_SESSION['payment_methods'][$productID] : 'Cash';
-
+                    // Display the selected product details
                     echo "<tr>";
-                    echo "<td>" . $row['ProductID'] . "</td>";
-                    echo "<td>" . $row['ProductName'] . "</td>";
-                    echo "<td>" . $row['ProductDescription'] . "</td>";
-                    echo "<td>" . $row['ProductCost'] . "</td>";
-                    echo "<td>" . $row['ProductQuantity'] . "</td>";
-                    echo '<td><img src="/B1801936/' . $row['ProductImage'] . '" width="150" height="100" /></td>';
-                    echo "<td>";
-                    
-                    echo '<form method="post" action="TotalAmount.php">';
-                    echo '<input type="hidden" name="ProductID[]" value="'. $row['ProductID']. '">';
-                    echo '<input type="hidden" name="ProductName[]" value="' . $row['ProductName'] . '">';
-                    echo '<input type="hidden" name="ProductCost[]" value="' . $row['ProductCost'] . '">';
-                    echo '<input type="hidden" name="ProductImage[]" value="' . $row['ProductImage'] . '">';
-        
-                    // Payment method selection
-                    echo '<label>Select Payment Method:</label>';
-                    echo '<select name="PaymentMethod[]">';
-                    echo '<option value="Cash" ' . ($paymentMethod === 'Cash' ? 'selected' : '') . '>Cash</option>';
-                    echo '<option value="Card" ' . ($paymentMethod === 'Card' ? 'selected' : '') . '>Credit Card</option>';
-                    echo '</select>';
-                
-                    // Submit button
-                    echo '<input type="submit" name="submit" value="Pay">';
-                    echo '</form>';
-                    
-                    echo '</td>';
-                    echo '</tr>';
-                }
-            } else {
-                echo "<tr><td colspan='8'>No products found</td></tr>";
-            }
+                    echo "<td>$selectedProductID</td>";
+                    echo "<td>$selectedProductName</td>";
+                    echo "<td>$selectedProductDescription</td>";
+                    echo "<td>$selectedProductCost</td>";
+                    echo "<td><img src='images/$selectedProductImage' alt='' width='100' height='100'></td>";
 
-            // Close the database connection
-            mysqli_close($conn);
-            ?>
-        </tbody>
-    </table>
+                    echo "<td><select name='quantity[]'>";
+                    for ($i = 1; $i <= 8; $i++) {
+                        echo "<option value='$i'>$i</option>";
+                    }
+                    echo "</select><br></td>";
+                    
+                    echo "<td><select name='PaymentType[]'>";
+                    echo "<option value='Cash'>Cash</option>";
+                    echo "<option value='Credit Card'>Credit Card</option>";
+                    echo "</select></td>";
+                    
+                    echo "<td><input type='date' name='PurchaseDate[]' required></td>";
+
+                    // Add hidden input fields to pass selected product details
+                    echo "<input type='hidden' name='ProductID[]' value='$selectedProductID'>";
+                    echo "<input type='hidden' name='ProductName[]' value='$selectedProductName'>";
+                    echo "<input type='hidden' name='ProductDescription[]' value='$selectedProductDescription'>";
+                    echo "<input type='hidden' name='ProductAmount[]' value='$selectedProductCost'>";
+                    echo "<input type='hidden' name='ProductImage[]' value='$selectedProductImage'>";
+
+                    echo "<td><input type='submit' name='submit' value='Pay'></td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </form>
+    </div>
+
     <footer class="footer">
         <div class="social-links">
+            <p>&copy; 2023 TheGetAway</p>
             <a href="https://www.facebook.com">Facebook</a>
             <a href="https://twitter.com">Twitter</a>
             <a href="https://www.instagram.com">Instagram</a>
         </div>
     </footer>
-</div>
-
 </body>
 </html>
-</div>
